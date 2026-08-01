4dd53c4fbe14465d85af4971f640524f57c90b25
Tharidu sadaruwan <tharidusadanuwan100@gmail.com>
2026-08-01 21:32:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from routers import yield_records
 
 #from routers import ai_prediction
 
-from routers import fertilizer_ai
+#from routers import fertilizer_ai
 
 from routers import disease_ai
 
@@ -142,7 +142,7 @@ def startup_event():
     start_scheduler()
 
     # AI Yield Prediction Scheduler
-    #start_prediction_scheduler()
+    start_prediction_scheduler()
 
     # AI Risk Analysis Scheduler
     start_risk_scheduler()
@@ -225,11 +225,11 @@ app.include_router(
 
 
 
-app.include_router(
+#app.include_router(
 
-    fertilizer_ai.router
+#    fertilizer_ai.router
 
-)
+#)
 
 
 
