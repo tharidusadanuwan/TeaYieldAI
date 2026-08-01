7c5d91a81647e3b1d526c7044c6b39a3eb0ded2c
Tharidu sadaruwan <tharidusadanuwan100@gmail.com>
2026-08-01 21:24:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ from routers import disease
 
 from routers import yield_records
 
-from routers import ai_prediction
+#from routers import ai_prediction
 
-#from routers import fertilizer_ai
+from routers import fertilizer_ai
 
 from routers import disease_ai
 
@@ -225,11 +225,11 @@ app.include_router(
 
 
 
-#app.include_router(
+app.include_router(
 
-#    fertilizer_ai.router
+    fertilizer_ai.router
 
-#)
+)
 
 
 
